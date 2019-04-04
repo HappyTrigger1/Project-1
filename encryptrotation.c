c7cda3ded7d2af0788bb5ed251c91c8e2450aec7
Thomas de Jager <thomas.dejager@uon.edu.au>
2019-04-04 03:55:34 +0000

    Set up basic function, mainly to understand how functions work.

--- encryptrotation.c
+++ encryptrotation.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+//e(x) = (m + k)(%26)    function required for encryption of message. 
+//d(c) = (c − k)(%26)    function required for decryption of message. 
+
+void simplecalculator();                    //function prototype
+
 int main(){
 
-int x;
-int y = 2;
-
-x = y + 4;
-
-printf("%d", x);
+    simplecalculator();                     //function in code
 
 return 0;
-
 }
+
+
+void simplecalculator(){                    //function definition
+    int x;
+    int y = 2;
+    x = y * 4;                              //calculation saying 
+    printf("%d", x);
+}
+    
