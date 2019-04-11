59cec713436ae5c823cea6f7a58f1ad0ff665bbf
Thomas de Jager <thomas.dejager@uon.edu.au>
2019-04-11 22:36:16 +0000

    Added code comments for better understanding of what's going on.  Encryption works by shifting letters across by 1, decryption is opposite.
    Unable to put any input into the panel as of yet.

--- encryptrotation.c
+++ encryptrotation.c
@@ -1,42 +1,36 @@
 //Rotation Cipher (Caesar Cipher)
 //Rotates letters ONE shift cross. i.e. a = b, b = c.....
-//Encrypt: HOW ARE YOU
-//Decryption: GNW BQD XNT
-//e(m) = (m + k)(%26)    function required for encryption of message. 
-//d(c) = (c − k)(%26)    function required for decryption of message.
-//m is the message, k is the key shift. 
-//e(m) is used to encrypt a message.
-//d(c) is used to decrypt a message. 
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
+//Function for decryption
 void decrypt(char arr[]){
       int i;
-      for(i = 0; i < strlen(arr); i++)
-      {
-            arr[i] = arr[i] + 10;
+      for(i = 0; i < strlen(arr); i++){
+            arr[i] = arr[i] - 1;                    //shifts letters back 1 for decryption
       }
 }
 
+//Function for encryption
 void encrypt(char arr[]){
       int i;
-      for(i = 0; i < strlen(arr); i++)
-      {
-            arr[i] = arr[i] - 10;
+      for(i = 0; i < strlen(arr); i++){
+            arr[i] = arr[i] + 1;                    //shifts letters across 1 for encryption
       }
 }
 
-int main()
-{
-      char word[40];	
+int main(){
+      char word[40];	                            //char data type
       int ch;
       printf("Enter a word:\t");
-      scanf("%s", word);
+      scanf("%s", word);                            //scan for a string, store in "word"
       printf("\nword:\t%s\n",word);
-      encrypt(word);
+      encrypt(word);                                //perform encryption function on "word"
       printf("\nEncrypted word:\t%s\n", word);
-      decrypt(word);
+      decrypt(word);                                //perform decryption function on ""
       printf("\nDecrypted word:\t%s\n", word);
       return 0;
 }
