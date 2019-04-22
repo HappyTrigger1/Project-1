745f76411ee22c119f6497549f11a0eb5b276546
Thomas de Jager <thomas.dejager@uon.edu.au>
2019-04-22 06:01:32 +0000

    This is now the WORKING code for a rotation cipher, using files. Code will read text from an input file, then perform encryption and decryption, and print the text back to a output file.

--- encryptrotation.c
+++ encryptrotation.c
@@ -8,40 +8,42 @@ void decryption(char arr[]);                           //Function prototype for 
 
 
 int main() {
+
+    FILE *input;                                       //Establishes a file input.txt 
+    char c[200];
+    int i = 0;
     
-    FILE *input;
-    FILE *output;
-    
-    input = fopen("input.txt", "r");
-    output = fopen("output.txt", "w");
+    input = fopen("input.txt", "r");                   //Sets up file input.txt for reading
     
     while(feof(input) == 0){
-        char c;
-        
-        fscanf(input, "%c", &c);
-        
-        encryption(&c);
-        
-        fprintf(output, "%c", c);
+        fscanf(input, "%c", &c[i]);                    //Scans words from input.txt file
+        i++;
     }
+
+    encryption(c);                                     //Encrypts the words from input.txt
+           
 }
 
 
 
-
-
-//Function for decryption
-void decryption(char arr[]){
-      int i;
-      for(i = 0; i < strlen(arr); i++){
-            arr[i] = arr[i] - 1;                    //shifts letters back 1 for decryption
-      }
-}
-
-//Function for encryption
+//Function for encryption/decryption
 void encryption(char arr[]){
-      int i;
-      for(i = 0; i < strlen(arr); i++){
-            arr[i] = arr[i] + 1;                    //shifts letters across 1 for encryption
-      }
+    FILE *output;                                    //Establishes a file output.txt
+    output = fopen("output.txt", "w");               //Sets up file output.txt for writing
+    int i;
+    fprintf(output,"Encrypted Message: ");
+    for(i = 0; i < strlen(arr); i++){
+        arr[i] = arr[i] + 1;                         //Shifts letters across 1 for encryption
+        fprintf(output, "%c", arr[i]);               //Writes the encrypted word to output.txt
+    }
+      
+    fprintf(output, "\nDecrypted Message: ");
+    int k;
+        for(k = 0; k < strlen(arr); k++){
+            arr[k] = arr[k] - 1;                     //shifts letters across 1 for encryption
+            fprintf(output, "%c", arr[k]);           //Writes the decrypted word to output.txt
+        }
 }
+
+
+
