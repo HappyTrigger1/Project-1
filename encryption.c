5bd7dee2c6ec2a9bf393db70c0c671d02ad0e245
Thomas de Jager <thomas.dejager@uon.edu.au>
2019-04-26 23:40:18 +0000

    Slight change to rotation cipher code. Code can now process 100000 characters instead of 100. Apple to process more words now.

--- encryption.c
+++ encryption.c
@@ -1,70 +1,52 @@
-#include<stdlib.h>
 #include<stdio.h>
-#include<wchar.h>
+#include<stdlib.h>
 #include<string.h>
- 
-#define ENCRYPT 0
-#define DECRYPT 1
-#define ALPHA 33
-#define OMEGA 126
 
 
- 
-int wideStrLen(wchar_t* str){
-	int i = 0;
-	while(str[i++]!=00);
- 
-	return i;
-}
+void encryption(char arr[]);                           //Function prototype for encrypting
+void decryption(char arr[]);                           //Function prototype for decrypting
 
- 
-void processFile(char* fileName,char plainKey, char cipherKey,int flag){
- 
-	FILE* inpFile = fopen(fileName,"r");
-	FILE* outFile;
- 
-	int i,len, diff = (flag==ENCRYPT)?(int)cipherKey - (int)plainKey:(int)plainKey - (int)cipherKey;
-	wchar_t str[1000], *outStr;
-	char* outFileName = (char*)malloc((strlen(fileName)+5)*sizeof(char));
- 
-	sprintf(outFileName,"%s_%s",fileName,(flag==ENCRYPT)?"ENC":"DEC");
- 
-	outFile = fopen(outFileName,"w");
- 
-	while(fgetws(str,1000,inpFile)!=NULL){
-		len = wideStrLen(str);
- 
-		outStr = (wchar_t*)malloc((len + 1)*sizeof(wchar_t));
- 
-		for(i=0;i<len;i++){
-			if((int)str[i]>=ALPHA && (int)str[i]<=OMEGA && flag == ENCRYPT)
-				outStr[i] = (wchar_t)((int)str[i]+diff);
-			 else if((int)str[i]-diff>=ALPHA && (int)str[i]-diff<=OMEGA && flag == DECRYPT)
-				outStr[i] = (wchar_t)((int)str[i]-diff);
-			else
-				outStr[i] = str[i];
-		}
-		outStr[i]=str[i];
- 
-		fputws(outStr,outFile);
- 
-		free(outStr);
-	}
- 
-	fclose(inpFile);
-	fclose(outFile);
+
+int main() {
+
+    FILE *input;                                       //Establishes a file input.txt
+    char c[100000];
+    int i = 0;
+    
+    input = fopen("input.txt", "r");                   //Sets up file input.txt for reading
+    
+    while(feof(input) == 0){
+        fscanf(input, "%c", &c[i]);                    //Scans words from input.txt file
+        i++;
+    }
+
+    encryption(c);                                     //Encrypts the words from input.txt
+        
 }
 
 
- 
-int main(int argC,char* argV[]){
-	if(argC!=5)
-		printf("Usage : %s <file name, plain key, cipher key, action (E)ncrypt or (D)ecrypt>",argV[0]);
-	else{
-		processFile(argV[1],argV[2][0],argV[3][0],(argV[4][0]=='E'||argV[4][0]=='e')?ENCRYPT:DECRYPT);
- 
-		printf("File %s_%s has been written to the same location as input file.",argV[1],(argV[4][0]=='E'||argV[4][0]=='e')?"ENC":"DEC");
-	}
- 
-	return 0;
+
+//Function for encryption/decryption
+void encryption(char arr[]){
+    FILE *output;                                     //Establishes a file output.txt
+    output = fopen("output.txt", "w");                //Sets up file output.txt for writing
+    int i;
+    fprintf(output,"Rotation Cipher");   
+    fprintf(output,"\nEncrypted (Rotation) Message: ");
+    
+    for(i = 0; i < strlen(arr); i++){
+        arr[i] = arr[i] + 1;                          //Shifts letters across 1 for encryption
+        fprintf(output, "%c", arr[i]);                //Writes the encrypted word to output.txt
+        }
+      
+    fprintf(output, "\nDecrypted (Rotation) Message: ");
+    int k;
+    
+    for(k = 0; k < strlen(arr); k++){
+        arr[k] = arr[k] - 1;                          //shifts letters across 1 for encryption
+        fprintf(output, "%c", arr[k]);                //Writes the decrypted word to output.txt
+        }
 }
+
+
+
